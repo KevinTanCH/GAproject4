a5b9ef6ae7696abf6cecae92814906ffffdacb41
kevintch <KevinTanChayHan@hotmail.com>
2024-04-24 13:05:41 +0800

    change register method to put

--- BACK/Booth/src/main/java/com/Booth/Booth/controller/BuyerController.java
+++ BACK/Booth/src/main/java/com/Booth/Booth/controller/BuyerController.java
@@ -5,10 +5,7 @@ import com.Booth.Booth.service.BuyerService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -18,7 +15,7 @@ public class BuyerController {
     private BuyerService buyerService;
 
     // Build Add Buyer REST API
-    @PostMapping
+    @PutMapping
     public ResponseEntity<BuyerDto> createBuyer(@RequestBody BuyerDto buyerDto){
         BuyerDto saveBuyer = buyerService.createBuyer(buyerDto);
         return new ResponseEntity<>(saveBuyer, HttpStatus.CREATED);
