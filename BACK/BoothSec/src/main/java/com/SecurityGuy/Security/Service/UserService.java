4451ff39c1d0a830aee22e5bc538d556a0e4ba55
kevintch <KevinTanChayHan@hotmail.com>
2024-04-29 15:12:44 +0800

    patch with password encoded

--- BACK/BoothSec/src/main/java/com/SecurityGuy/Security/Service/UserService.java
+++ BACK/BoothSec/src/main/java/com/SecurityGuy/Security/Service/UserService.java
@@ -5,6 +5,7 @@ import com.SecurityGuy.Security.entity.User;
 import com.SecurityGuy.Security.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,6 +14,13 @@ import java.util.Optional;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public User post1Seller(Long sellerId) {
         return userRepository.findById(sellerId)
@@ -32,7 +40,7 @@ public class UserService {
         userToBePatched.setName(requstBodyUser.getName());
         userToBePatched.setUsername(requstBodyUser.getUsername());
         userToBePatched.setEmail(requstBodyUser.getEmail());
-        userToBePatched.setPassword(requstBodyUser.getPassword());
+        userToBePatched.setPassword(passwordEncoder.encode(requstBodyUser.getPassword()));
         userToBePatched.setPhoto(requstBodyUser.getPhoto());
         userToBePatched.setLocation(requstBodyUser.getLocation());
         userToBePatched.setIsActive(requstBodyUser.getIsActive());
